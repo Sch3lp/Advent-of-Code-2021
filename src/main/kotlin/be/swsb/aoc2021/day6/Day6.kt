aec22bad9955a671b663512193e2a8182b98ed1b
Tim Schraepen <sch3lp@gmail.com>
2021-12-07 22:10:44 +0100

    ⭐️⭐️ Solved Day 6 part 2. Couldn't have done it without Amarth86. GG's!

--- src/main/kotlin/be/swsb/aoc2021/day6/Day6.kt
+++ src/main/kotlin/be/swsb/aoc2021/day6/Day6.kt
@@ -1,16 +1,17 @@
 package be.swsb.aoc2021.day6
 
 object Day6 {
+
     fun solve1(input: List<Int>, days: Int): Int {
         return dayCycle(input, days).size
     }
-    fun solve2(input: List<String>): Int {
-        return 0
+    fun solve2(input: List<Int>, days: Int): Long {
+        return dayCyclePart2(input, days)
     }
 }
 
 fun dayCycle(school: List<Int>, days: Int): List<Int> {
-    return (1..days).fold(school) { acc, day -> acc.dayPasses() }
+    return (1..days).fold(school) { acc, _ -> acc.dayPasses() }
 }
 
 fun List<Int>.dayPasses(): List<Int> {
@@ -18,3 +19,23 @@ fun List<Int>.dayPasses(): List<Int> {
     val newSpawns: Sequence<Int> = generateSequence { 8 }.take(amountOfNewSpawns)
     return map { if (it == 0) 6 else it - 1 } + newSpawns.toList()
 }
+
+fun dayCyclePart2(initialSchool: List<Int>, days: Int): Long {
+    val fishGroupedByIncubationTime: Map<IncubationTime, Long> = (0..8).associateWith { incubationTime ->
+        initialSchool.count { it == incubationTime }.toLong()
+    }
+
+    return (1..days).fold(fishGroupedByIncubationTime) { acc, _ -> acc.dayPasses() }.values.sum()
+}
+
+fun Map<IncubationTime, Long>.dayPasses(): Map<IncubationTime, Long> {
+    return map { (k, _) ->
+        when (k) {
+            6 -> k to (this[k+1]!! + this[0]!!)
+            8 -> k to this[0]!!
+            else -> k to this[k+1]!!
+        }
+    }.toMap()
+}
+
+typealias IncubationTime = Int
--- src/test/kotlin/be/swsb/aoc2021/day6/Day6Test.kt
+++ src/test/kotlin/be/swsb/aoc2021/day6/Day6Test.kt
@@ -18,24 +18,47 @@ class Day6Test {
     }
 
     @Test
-    fun `Part 1's actual input should result in 456`() {
+    fun `Part 1's actual input should result in 390923`() {
         val input = "day6/actualInput.txt".readLines().flatMap { line -> line.split(",").map { it.toInt() } }
         val actual = Day6.solve1(input, 80)
         assertThat(actual).isEqualTo(390923)
     }
 
+    @Test
+    fun `Part 2's test input should result in 26984457539`() {
+        val input = "day6/testInput.txt".readLines().flatMap { line -> line.split(",").map { it.toInt() } }
+        val actual = Day6.solve2(input, 256)
+        assertThat(actual).isEqualTo(26984457539L)
+    }
+
+    @Test
+    fun `Part 2's actual input should result in 1749945484935`() {
+        val input = "day6/actualInput.txt".readLines().flatMap { line -> line.split(",").map { it.toInt() } }
+        val actual = Day6.solve2(input, 256)
+        assertThat(actual).isEqualTo(1749945484935L)
+    }
+
     @Nested
-    inner class SomeUnitTests {
+    inner class Part1Tests {
         @Test
         fun `Given an lanternfish at 1, when the day passes twice, one new lanternfish at 8 spawns, and the other lanternfish is reset to 6`() {
             listOf(1)
                 .dayPasses()
                 .also { assertThat(it).containsExactly(0) }
                 .dayPasses()
-                .also{ assertThat(it).containsExactly(6, 8) }
+                .also { assertThat(it).containsExactly(6, 8) }
 
-            val actual = dayCycle(listOf(1),2)
+            val actual = dayCycle(listOf(1), 2)
             assertThat(actual).containsExactly(6, 8)
+        }
+    }
+
+    @Nested
+    inner class Part2Tests {
+        @Test
+        fun `Given an lanternfish at 1, when the day passes twice, one new lanternfish at 8 spawns, and the other lanternfish is reset to 6`() {
+            val actual = dayCyclePart2(listOf(3, 4, 3, 1, 2), 4)
+            assertThat(actual).isEqualTo(9L)
         }
     }
 }
