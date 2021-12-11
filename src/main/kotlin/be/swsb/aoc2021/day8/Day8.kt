fc0c1454b36802fe01f2c941ce4b5dfcf7a097ba
Tim Schraepen <sch3lp@gmail.com>
2021-12-11 18:53:55 +0100

    ⭐️⭐️ Solved Day 8 part 2.

--- src/main/kotlin/be/swsb/aoc2021/day8/Day8.kt
+++ src/main/kotlin/be/swsb/aoc2021/day8/Day8.kt
@@ -1,5 +1,6 @@
 package be.swsb.aoc2021.day8
 
+
 private const val amountOfSignalsToDisplay1 = 2
 private const val amountOfSignalsToDisplay4 = 4
 private const val amountOfSignalsToDisplay7 = 3
@@ -22,6 +23,105 @@ object Day8 {
     }
 
     fun solve2(input: List<String>): Int {
-        TODO()
+        return input.sumOf { line ->
+            val (signalPattern, displaysAsString) = line.split("| ")
+            val displays = displaysAsString.split(" ")
+            val decoder = deduce(signalPattern).build()
+            displays.joinToString("") { encodedDisplay -> decoder.decode(encodedDisplay) }.toInt()
+        }
+    }
+
+    fun deduce(signalPattern: String): DecoderBuilder {
+        val encodedDigits = signalPattern.split(" ")
+        val encodedOne = encodedDigits.first { it.count() == 2 }
+        val encodedSeven = encodedDigits.first { it.count() == 3 }
+        val encodedFour = encodedDigits.first { it.count() == 4 }
+
+        return DecoderBuilder()
+            .deduceSegmentA(encodedOne, encodedSeven)
+            .deduceOtherSegments(encodedDigits, encodedFour)
+    }
+
+
+}
+
+class DecoderBuilder(private val map: Map<Signal, Segment> = emptyMap()) {
+    fun with(mapping: Pair<Signal, Segment>): DecoderBuilder {
+        return DecoderBuilder(map + listOf(mapping).toMap())
+    }
+
+    fun deduceSegmentA(
+        encodedOne: String,
+        encodedSeven: String
+    ): DecoderBuilder {
+        val signalForSegmentA = encodedSeven.diff(encodedOne)
+        return with(signalForSegmentA[0] to 'a')
+    }
+
+    /*
+    If we add (overlay) all encoded digits on top of each other, and then add (overlay) the encoded 4 exactly 2 more times we get unique amounts for all segments
+    The top segment we already know.
+     0 + 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + 4 + 4 - segmentA
+      ----
+     8    10
+     8    10
+      9999
+     4    11
+     4    11
+      7777
+     */
+    fun deduceOtherSegments(encodedDigits: List<String>, encodedFour: String): DecoderBuilder {
+        val occurrencesPerSignal = (encodedDigits + encodedFour + encodedFour)
+            .joinToString("")
+            .replace(getSignal('a').toString(), "") //drop the signal for the segment we already know before grouping
+            .groupBy { it }
+            .map { (k, v) -> v.count() to k }
+            .toMap()
+        return with(occurrencesPerSignal[8]!! to 'b')
+            .with(occurrencesPerSignal[10]!! to 'c')
+            .with(occurrencesPerSignal[9]!! to 'd')
+            .with(occurrencesPerSignal[4]!! to 'e')
+            .with(occurrencesPerSignal[11]!! to 'f')
+            .with(occurrencesPerSignal[7]!! to 'g')
+    }
+
+    fun build(): Decoder {
+        return Decoder(map)
+    }
+
+    fun getSignal(segment: Segment): Signal {
+        return map.filterValues { it == segment }.keys.first()
+    }
+
+    private fun String.diff(other: String) = this.filter { it !in other.toList() }
+}
+
+
+class Decoder(private val map: Map<Signal, Segment>) {
+
+    init {
+        check(map.keys.containsAll(('a'..'g').toList())) { "Tried building a Decoder with incomplete or incorrect signals: ${map.keys}" }
+        check(map.values.containsAll(('a'..'g').toList())) { "Tried building a Decoder with incomplete or incorrect segments: ${map.values}" }
+    }
+
+    private val digits: Map<String, String> = listOf(
+        "abcefg" to "0",
+        "cf" to "1",
+        "acdeg" to "2",
+        "acdfg" to "3",
+        "bcdf" to "4",
+        "abdfg" to "5",
+        "abdefg" to "6",
+        "acf" to "7",
+        "abcdefg" to "8",
+        "abcdfg" to "9",
+    ).toMap()
+
+    fun decode(encodedDisplay: CharSequence): Digit {
+        val decodedDisplay = encodedDisplay.map { map[it] }.sortedBy { it }.joinToString("")
+        return digits[decodedDisplay]!!
     }
 }
+typealias Digit = String
+typealias Signal = Char
+typealias Segment = Char
--- src/test/kotlin/be/swsb/aoc2021/day8/Day8Test.kt
+++ src/test/kotlin/be/swsb/aoc2021/day8/Day8Test.kt
@@ -1,21 +1,83 @@
 package be.swsb.aoc2021.day8
 
 import be.swsb.aoc2021.common.readLines
+import be.swsb.aoc2021.day8.Day8.deduce
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 
 
 class Day8Test {
+    @Nested
+    inner class SolutionTests {
+        @Test
+        fun `solve1 with test input returns 26`() {
+            val actual = Day8.solve1("day8/testInput.txt".readLines())
+            assertThat(actual).isEqualTo(26)
+        }
 
-    @Test
-    fun `solve1 with test input returns 26`() {
-        val actual = Day8.solve1("day8/testInput.txt".readLines())
-        assertThat(actual).isEqualTo(26)
+        @Test
+        fun `solve1 with actual input returns 349`() {
+            val actual = Day8.solve1("day8/actualInput.txt".readLines())
+            assertThat(actual).isEqualTo(349)
+        }
+
+        @Test
+        fun `solve2 with test input returns 61229`() {
+            val actual = Day8.solve2("day8/testInput.txt".readLines())
+            assertThat(actual).isEqualTo(61229)
+        }
+
+        @Test
+        fun `solve2 with actual input returns 1070957`() {
+            val actual = Day8.solve2("day8/actualInput.txt".readLines())
+            assertThat(actual).isEqualTo(1070957)
+        }
     }
 
-    @Test
-    fun `solve1 with actual input returns 349`() {
-        val actual = Day8.solve1("day8/actualInput.txt".readLines())
-        assertThat(actual).isEqualTo(349)
+    @Nested
+    inner class DecoderTests {
+        @Test
+        fun `Given example mapping, can decode signals to digits`() {
+            val decoder = DecoderBuilder()
+                .with('d' to 'a')
+                .with('e' to 'b')
+                .with('a' to 'c')
+                .with('f' to 'd')
+                .with('g' to 'e')
+                .with('b' to 'f')
+                .with('c' to 'g')
+                .build()
+
+            val digit: Digit = decoder.decode("dab")
+            val digit2: Digit = decoder.decode("ab")
+
+            assertThat(digit).isEqualTo(7)
+            assertThat(digit2).isEqualTo(1)
+        }
+    }
+
+    @Nested
+    inner class DeductionTests {
+        @Test
+        fun `Given signals for 7 and 1, can deduce the 'a' segment`() {
+            val decoderBuilder: DecoderBuilder = deduce("dab ab")
+
+            val signal: Signal = decoderBuilder.getSignal('a')
+            assertThat(signal).isEqualTo('d')
+        }
+
+        @Test
+        fun `Given example signal pattern, can build the expected decoder`() {
+            val decoderBuilder: DecoderBuilder = deduce("acedgfb cdfbe gcdfa fbcad dab cefabd cdfgeb eafb cagedb ab")
+
+            assertThat(decoderBuilder.getSignal('a')).isEqualTo('d')
+            assertThat(decoderBuilder.getSignal('b')).isEqualTo('e')
+            assertThat(decoderBuilder.getSignal('c')).isEqualTo('a')
+            assertThat(decoderBuilder.getSignal('d')).isEqualTo('f')
+            assertThat(decoderBuilder.getSignal('e')).isEqualTo('g')
+            assertThat(decoderBuilder.getSignal('f')).isEqualTo('b')
+            assertThat(decoderBuilder.getSignal('g')).isEqualTo('c')
+        }
     }
 }
